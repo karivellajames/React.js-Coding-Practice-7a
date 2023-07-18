c6afe26f30c27f70d1c19cc675fb6b7886d2467a
karivellajames <karivella.james2022@gmail.com>
2023-07-18 12:36:07 +0000

    Cash Withdrawal

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {DenominationDetails, updateBalance} = props
+  const {value} = DenominationDetails
+
+  const onClickDenomination = () => {
+    updateBalance(value)
+  }
+
+  return (
+    <li className="denomination-item">
+      <button
+        type="button"
+        className="denomination-button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
